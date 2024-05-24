b7340657cac2e972e6a93e60c19672dda4661a15
tracyyma <tracyyma@tencent.com>
2024-05-24 17:46:35 +0800

    feat: 增加AIOPS适配数据； # Reviewed, transaction id: 8673

---
+++ PatchTST_supervised/data_explore/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/5/24 17:44
--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -1,23 +1,38 @@
-from data_provider.data_factory import data_provider
-from exp.exp_basic import Exp_Basic
-from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST
-from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
-from utils.metrics import metric
-
-import numpy as np
-import torch
-import torch.nn as nn
-from torch import optim
-from torch.optim import lr_scheduler 
-
 import os
 import time
-
 import warnings
-import matplotlib.pyplot as plt
+import torch
 import numpy as np
+import torch.nn as nn
+from torch import optim
+from torch.optim import lr_scheduler
+import matplotlib.pyplot as plt
+from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST
+from exp.exp_basic import Exp_Basic
+from data_provider.data_factory import data_provider
+from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
+from utils.metrics import metric
+from data_explore.plot_predict import plot_predict_result
 
 warnings.filterwarnings('ignore')
+
+
+def plot_optim_adam(scheduler):
+    # """迭代器，嵌入影响模型训练，研究代码使用"""
+    def plot_fig(x, y):
+        plt.figure(figsize=(15, 8))
+        plt.plot(x, y, label='OneCycle')
+        plt.savefig("dd.png", dpi=300)
+        plt.close()
+
+    lrs, steps = [], []
+    for epoch in range(100):
+        for batch in range(64):
+            scheduler.step()
+            lrs.append(scheduler.get_lr()[0])
+            steps.append(epoch * 64 + batch)
+    plot_fig(steps, lrs)
+
 
 class Exp_Main(Exp_Basic):
     def __init__(self, args):
@@ -36,11 +51,13 @@ class Exp_Main(Exp_Basic):
         model = model_dict[self.args.model].Model(self.args).float()
 
         if self.args.use_multi_gpu and self.args.use_gpu:
-            model = nn.DataParallel(model, device_ids=self.args.device_ids)
+            model = nn.DataParallel(model,
+                                    device_ids=self.args.device_ids)  # 在多卡GPU上，当迭代次数或epoch足够大，使用nn.DataParallel进行多个GPU来加速训练；
         return model
 
-    def _get_data(self, flag):
-        data_set, data_loader = data_provider(self.args, flag)
+    def _get_data(self, flag, **kwargs):
+        """获取数据，具体实现数据的入口"""
+        data_set, data_loader = data_provider(self.args, flag, **kwargs)
         return data_set, data_loader
 
     def _select_optimizer(self):
@@ -98,14 +115,16 @@ class Exp_Main(Exp_Basic):
         return total_loss
 
     def train(self, setting):
+        print(f"获取数据序列: ")
         train_data, train_loader = self._get_data(flag='train')
         vali_data, vali_loader = self._get_data(flag='val')
         test_data, test_loader = self._get_data(flag='test')
 
+        # checkpoint location
         path = os.path.join(self.args.checkpoints, setting)
         if not os.path.exists(path):
             os.makedirs(path)
-
+        print("start train ")
         time_now = time.time()
 
         train_steps = len(train_loader)
@@ -114,14 +133,14 @@ class Exp_Main(Exp_Basic):
         model_optim = self._select_optimizer()
         criterion = self._select_criterion()
 
-        if self.args.use_amp:
+        if self.args.use_amp:  # 自动混合精度，可以在神经网络推理过程中，针对不同的层，采用不同的数据精度进行计算，节省显存和加快速度
             scaler = torch.cuda.amp.GradScaler()
-            
-        scheduler = lr_scheduler.OneCycleLR(optimizer = model_optim,
-                                            steps_per_epoch = train_steps,
-                                            pct_start = self.args.pct_start,
-                                            epochs = self.args.train_epochs,
-                                            max_lr = self.args.learning_rate)
+
+        scheduler = lr_scheduler.OneCycleLR(optimizer=model_optim,
+                                            steps_per_epoch=train_steps,
+                                            pct_start=self.args.pct_start,
+                                            epochs=self.args.train_epochs,
+                                            max_lr=self.args.learning_rate)
 
         for epoch in range(self.args.train_epochs):
             iter_count = 0
@@ -129,13 +148,14 @@ class Exp_Main(Exp_Basic):
 
             self.model.train()
             epoch_time = time.time()
-            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(
+                    train_loader):  # batch_x:[batch-size, seq-len, 1], batch_y:[batch-size, pre-len, 1], 这里的1指得是序列维度；
                 iter_count += 1
                 model_optim.zero_grad()
-                batch_x = batch_x.float().to(self.device)
 
+                batch_x = batch_x.float().to(self.device)
                 batch_y = batch_y.float().to(self.device)
-                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_x_mark = batch_x_mark.float().to(self.device)  # batch_x_mark 时间编码；
                 batch_y_mark = batch_y_mark.float().to(self.device)
 
                 # decoder input
@@ -156,15 +176,15 @@ class Exp_Main(Exp_Basic):
                         f_dim = -1 if self.args.features == 'MS' else 0
                         outputs = outputs[:, -self.args.pred_len:, f_dim:]
                         batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
-                        loss = criterion(outputs, batch_y)
+                        loss = criterion(outputs, batch_y)  # LOSS-MSE；
                         train_loss.append(loss.item())
                 else:
                     if 'Linear' in self.args.model or 'TST' in self.args.model:
-                            outputs = self.model(batch_x)
+                        outputs = self.model(batch_x)
                     else:
                         if self.args.output_attention:
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                            
+
                         else:
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark, batch_y)
                     # print(outputs.shape,batch_y.shape)
@@ -174,7 +194,7 @@ class Exp_Main(Exp_Basic):
                     loss = criterion(outputs, batch_y)
                     train_loss.append(loss.item())
 
-                if (i + 1) % 100 == 0:
+                if (i + 1) % 100 == 0:  # 有每个epoch中，每100次输出当前的loss + 预测速度[平均每次迭代花费时间]，及预计要花费的时间；
                     print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
                     speed = (time.time() - time_now) / iter_count
                     left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
@@ -189,8 +209,8 @@ class Exp_Main(Exp_Basic):
                 else:
                     loss.backward()
                     model_optim.step()
-                    
-                if self.args.lradj == 'TST':
+
+                if self.args.lradj == 'TST':  # 学习率衰减
                     adjust_learning_rate(model_optim, scheduler, epoch + 1, self.args, printout=False)
                     scheduler.step()
 
@@ -201,6 +221,7 @@ class Exp_Main(Exp_Basic):
 
             print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
                 epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+
             early_stopping(vali_loss, self.model, path)
             if early_stopping.early_stop:
                 print("Early stopping")
@@ -210,7 +231,7 @@ class Exp_Main(Exp_Basic):
                 adjust_learning_rate(model_optim, scheduler, epoch + 1, self.args)
             else:
                 print('Updating learning rate to {}'.format(scheduler.get_last_lr()[0]))
-
+        # save model;
         best_model_path = path + '/' + 'checkpoint.pth'
         self.model.load_state_dict(torch.load(best_model_path))
 
@@ -218,11 +239,11 @@ class Exp_Main(Exp_Basic):
 
     def test(self, setting, test=0):
         test_data, test_loader = self._get_data(flag='test')
-        
+
         if test:
             print('loading model')
             self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
-
+        print("start testing...")
         preds = []
         trues = []
         inputx = []
@@ -254,7 +275,7 @@ class Exp_Main(Exp_Basic):
                                 outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
                 else:
                     if 'Linear' in self.args.model or 'TST' in self.args.model:
-                            outputs = self.model(batch_x)
+                        outputs = self.model(batch_x)
                     else:
                         if self.args.output_attention:
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
@@ -282,7 +303,7 @@ class Exp_Main(Exp_Basic):
                     visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
 
         if self.args.test_flop:
-            test_params_flop((batch_x.shape[1],batch_x.shape[2]))
+            test_params_flop((batch_x.shape[1], batch_x.shape[2]))
             exit()
         preds = np.array(preds)
         trues = np.array(trues)
@@ -326,12 +347,13 @@ class Exp_Main(Exp_Basic):
         with torch.no_grad():
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
                 batch_x = batch_x.float().to(self.device)
-                batch_y = batch_y.float()
+                batch_y = batch_y.float().to(self.device)  # batch_y.float()
                 batch_x_mark = batch_x_mark.float().to(self.device)
                 batch_y_mark = batch_y_mark.float().to(self.device)
 
                 # decoder input
-                dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[2]]).float().to(batch_y.device)
+                dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[2]]).float().to(
+                    batch_y.device)
                 dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
                 # encoder - decoder
                 if self.args.use_amp:
@@ -352,16 +374,195 @@ class Exp_Main(Exp_Basic):
                         else:
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
                 pred = outputs.detach().cpu().numpy()  # .squeeze()
-                preds.append(pred)
+                preds.append(pred_data.inverse_transform(pred))
 
         preds = np.array(preds)
         preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
 
+        # predict-result save
+        folder_path = './pred_results/' + setting + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        np.save(folder_path + 'real_prediction.npy', preds)
+        return
+
+    def predict_online(self, setting, load=True):
+        """
+        在线预测
+        """
+        pred_data, pred_loader = self._get_data(flag='pred')
+
+        if load:
+            path = os.path.join(self.args.checkpoints, setting)
+            best_model_path = path + '/' + 'checkpoint.pth'
+            self.model.load_state_dict(torch.load(best_model_path))
+
+        preds = []
+        trues = []
+        y_stamps = []
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_stamp) in enumerate(pred_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[2]]).float().to(
+                    batch_y.device)
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        if 'Linear' in self.args.model or 'TST' in self.args.model:
+                            outputs = self.model(batch_x)
+                        else:
+                            if self.args.output_attention:
+                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                            else:
+                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                else:
+                    if 'Linear' in self.args.model or 'TST' in self.args.model:
+                        outputs = self.model(batch_x)
+                    else:
+                        if self.args.output_attention:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                        else:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                pred = outputs.detach().cpu().numpy()  # .squeeze()
+                true = batch_y.detach().cpu().numpy()  # .squeeze()
+                # print(outputs.shape, pred.shape, batch_y.shape, true.shape, batch_y_stamp.shape)
+
+                preds.append(pred_data.inverse_transform(pred))  #
+                trues.append(pred_data.inverse_transform(true))  #
+                y_stamps.append(batch_y_stamp)
+
+        preds = np.array(preds)
+        trues = np.array(trues)
+        y_stamps = torch.cat(y_stamps, dim=0).numpy()  # 按行拼接；
+
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+        y_stamps = y_stamps.reshape(-1, y_stamps.shape[-2], y_stamps.shape[-1])
         # result save
-        folder_path = './results/' + setting + '/'
+        folder_path = './pred_results/' + setting + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        # save
+        title_prefix = pred_data.test_data.split('.')[0]
+        np.save(folder_path + f"{title_prefix}_real_prediction.npy", [preds, trues, y_stamps])
+        plot_predict_result(preds_result=[preds, trues, y_stamps],
+                            source_ts_csv=os.path.join(pred_data.root_path, pred_data.data_path, pred_data.test_data),
+                            title=f"{title_prefix}")
+        return
+
+    def __predict_online(self, setting, test_data):
+        """
+        预测&可视化；
+        """
+
+        pred_data, pred_loader = self._get_data(flag='pred', test_data=test_data)
+
+        preds = []
+        trues = []
+        y_stamps = []
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark, batch_y_stamp) in enumerate(pred_loader):
+                # print("======>", batch_x.shape)
+                batch_x = batch_x.float().to(self.device)  # batch_size * seq_len *
+                batch_y = batch_y.float().to(self.device)
+                # print("<0>", batch_x.shape, batch_x_mark.shape)
+
+                # decoder input
+                dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[2]]).float().to(self.device)
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+
+                # print(dec_inp.shape, self.args.use_amp)
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        if 'Linear' in self.args.model or 'TST' in self.args.model:
+                            outputs = self.model(batch_x)
+                        else:
+                            batch_x_mark = batch_x_mark.float().to(self.device)  # 非常耗时；
+                            batch_y_mark = batch_y_mark.float().to(self.device)
+                            if self.args.output_attention:
+                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                            else:
+                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                else:
+                    # print("======>", self.model) # Linear;
+                    if 'Linear' in self.args.model or 'TST' in self.args.model:
+                        print(f"输入：{batch_x.shape}")
+                        outputs = self.model(batch_x)
+                    else:
+                        if self.args.output_attention:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                        else:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                        print("<1>", outputs)
+                pred = outputs.detach().cpu().numpy().squeeze(0)
+                true = batch_y.detach().cpu().numpy().squeeze(0)
+                # print(outputs.shape, pred.shape, batch_y.shape, true.shape, batch_y_stamp.shape)
+                # print(f"预测结果：{pred.shape}")
+                preds.append(pred_data.inverse_transform(pred))  # 预测结果-反 标准化；pred=(1.1440.1)
+                trues.append(pred_data.inverse_transform(true))  #
+                y_stamps.append(batch_y_stamp)
+
+        preds = np.array(preds)
+        trues = np.array(trues)
+        y_stamps = torch.cat(y_stamps, dim=0).numpy()  # 按行拼接；
+
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+        y_stamps = y_stamps.reshape(-1, y_stamps.shape[-2], y_stamps.shape[-1])
+
+        # result save
+        folder_path = './pred_results/' + setting + '/'
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        np.save(folder_path + 'real_prediction.npy', preds)
-
+        # save predict to npy;
+        title_prefix = test_data.replace('.csv', '')
+        out_filename = f"{folder_path}{title_prefix}_real_prediction.npy"
+        np.save(out_filename, [preds, trues, y_stamps])
+        print(f"预测结果存储：{out_filename}")
+        # plot file;
+        plot_predict_result(preds_result=[preds, trues, y_stamps],
+                            source_ts_csv=os.path.join(pred_data.root_path, pred_data.data_path, test_data),
+                            title=f"{title_prefix}", isload=False)
         return
+
+    def batch_online_predict(self, setting, load=True):
+        """
+        根据配置的测试列表数据，进行批量预测
+        :param setting:
+        :return:
+        """
+        from tqdm import tqdm
+        self.args.is_training = 0
+
+        from data_provider.data_api import select_samples
+        # load model；
+        if load:
+            path = os.path.join(self.args.checkpoints, setting)
+            best_model_path = path + '/' + 'checkpoint.pth'
+            self.model.load_state_dict(torch.load(best_model_path))
+
+        # pred_data, pred_loader = self._get_data(flag='pred')
+        test_file_list = select_samples(self.args.test_data_list, root_path=self.args.root_path,
+                                        data_path=self.args.data_path,
+                                        metric_type='ALL')
+        # batch predict
+        for i in tqdm(range(len(test_file_list))):
+            try:
+                print(f"{i}: {test_file_list[i]}", )
+                self.__predict_online(setting, test_data=test_file_list[i])
+            except Exception as e:
+                print(f"error: {e}")
+                continue
